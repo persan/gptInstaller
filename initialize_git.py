8152b4d674dbf24a671e9db173ed8cb6006e834b
persan <per.s.sandberg@bahnhof.se>
2019-11-04 22:12:19 +0100

    Initial commit in project creation.

--- initialize_git.py
+++ initialize_git.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import subprocess
-subprocess.call(["git", "init", "."])
-subprocess.call(["git", "add",  "."])
-subprocess.call(["git", "commit", "-mInitial commit in project creation."])
+print(subprocess.call(["git", "init", "."]))
+print(subprocess.call(["git", "add",  "."]))
+print(subprocess.call(["git", "commit", "-mInitial commit in project creation."]))
 print("----------------")
